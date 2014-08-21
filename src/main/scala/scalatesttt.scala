f327889ba89597704b221333f15207961dce41e4
IChocolateKapa <echochocolatekapa@163.com>
2014-08-21 11:30:57 +0800

    Update scalatesttt.scala

--- src/main/scala/scalatesttt.scala
+++ src/main/scala/scalatesttt.scala
@@ -1,16 +1,36 @@
 package scalatest
 
+import org.apache.spark.SparkContext
+
+import org.apache.spark.SparkContext._
+
+import org.apache.spark.SparkConf
+
 import com.datastax.spark.connector._
 
 import com.datastax.spark.connector.streaming._
 
-object Testobj extends App {
+object TestExample {
+  def main(args: Array[String]) {
+    val conf = new SparkConf(true).set("spark.cassandra.connection.host", "127.0.0.1")
 
-  def isPalindrome(word: String) = {
-    val modifiedWord = word.toLowerCase.replaceAll("[^a-z0-9]", "")
-    val reversed = modifiedWord.reverse()
-    modifiedWord == reversed
+	val sc = new SparkContext("spark://127.0.0.1:7077", "test", conf)
+
+	val ssc = new StreamingContext(conf, Seconds(n))
+
+	val stream = ssc.actorStream[String](Props[SimpleStreamingActor], actorName, StorageLevel.MEMORY_AND_DISK)
+
+	val wc = stream.flatMap(_.split("\\s+")).map(x => (x, 1)).reduceByKey(_ + _).saveToCassandra("streaming_test", "words", SomeColumns("word", "count"))
+
+	val rdd = sc.cassandraTable("test", "kv")
+
+	println(rdd.count)
+
+	println(rdd.first)
+
+	println(rdd.map(_.getInt("value")).sum)
+
+	val collection = sc.parallelize(Seq(("key3", 3), ("key4", 4)))
+	collection.saveToCassandra("test", "kv", SomeColumns("key", "value"))
   }
-
-  println("Is 'Herculaneum' a palindrome? " + isPalindrome("Herculaneum"))
 }
